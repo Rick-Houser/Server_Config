ba557da51c97c860b40611b61ee9f5ff0c244521
FrackenToaster <frakentoaster@gmail.com>
2016-11-07 09:25:01 -0800

    Init commit. Add working script and start of readme.

--- ricks_config_tool.sh
+++ ricks_config_tool.sh
@@ -1,31 +1,95 @@
 #!/usr/bin/env bash
 
-# This script will setup and configure LAMP stack to run a simple PHP web app
+# Instructions to use this script:
+#
+# chmod +x SCRIPTNAME.sh
+#
+# sudo ./SCRIPTNAME.sh
+
+# Notes about this script:
+# This tool will check for updates and existing files before proceeding to setup
+# and configure LAMP stack to run a simple PHP web app.
 
 # Check for package manager updates
-sudo apt-get update
+sudo apt-get update -q
 
-# Install Apache
-sudo apt-get install apache2
+# --------------------------
+# Remove debian packages
+# --------------------------
+# declare -a remove_packages=(
+#   "php5"
+#   "libapache2-mod-php5"
+#   "php5-mcrypt"
+# )
+# for rm_pkg in "${remove_packages[@]}"
+# do
+#   if dpkg -l | grep "${rm_pkg}"; then
+#     sudo apt-get purge -y "${rm_pkg}"
+#     sudo rm -rf /etc/"${rm_pkg}" /var/lib/"${rm_pkg}"
+#     sudo apt-get autoremove -y
+#     sudo apt-get autoclean -y
+#   fi
+# done
 
-# Install MySQL and a helper package
-sudo apt-get install mysql-server php5-mysql
+# --------------------------
+# Install debian packages
+# --------------------------
+declare -a install_packages=(
+  "apache2"
+  "mysql-server"
+  "php5-mysql"
+  "php5"
+  "libapache2-mod-php5"
+  "php5-mcrypt"
+)
+for in_pkg in "${install_packages[@]}"
+do
+  if ! dpkg -l | grep "${in_pkg}"; then
+    sudo apt-get install -y "${in_pkg}"
+    if [ "${in_pkg}" == "mysql-server" ]; then
+      # Create the MySQL database
+      sudo mysql_install_db
 
-# Create the MySQL database
-sudo mysql_install_db
+      # Secure our installation by removing insecure defaults
+      sudo mysql_secure_installation
+    fi
+  fi
+done
 
-# Secure our installation by removing insecure defaults
-sudo mysql_secure_installation
+# Replace default index.html with index.php
+if [ -f "/var/www/html/index.html" ]; then
+  sudo rm /var/www/html/index.html
+  touch /var/www/html/index.php
+fi
 
-# Install PHP and helper packages
-sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+# --------------------------
+# Setting metadata and content
+# --------------------------
 
-# Remove default .html file and replace it with our .php file
-sudo rm /var/www/html/index.html
-sudo touch /var/www/html/index.php
+# To change metadata for files or folders, use the following example as a guide.
 
-# Add requested php content to file
-echo '<?php header("Content-Type: text/plain"); echo "Hello, world!\n";' >> /va$
+# Set metadata for a given file or directory
+declare -A metadata=(
+  ["permissions"]="644"
+  ["owner"]="root"
+  ["group"]="staff"
+  ["file"]="/var/www/html/index.php"
+  ["content"]='<?php header("Content-Type: text/plain"); echo "Hello, world!\n";'
+)
+sudo chmod "${metadata[permissions]}" "${metadata[file]}"
+sudo chown "${metadata[owner]}" "${metadata[file]}"
+sudo chgrp "${metadata[group]}" "${metadata[file]}"
+sudo echo "${metadata[content]}" > "${metadata[file]}"
 
-# Restart apache service
+# Changes permissions for all files and folders within a given directory
+# declare -A dir_metadata=(
+#   ["directory_permissions"]="755"
+#   ["file_permissions"]="644"
+#   ["directory"]="path/to/directory/" )
+# sudo find "${dir_metadata[directory]}" -type f -print0 | xargs -0 sudo chmod "${dir_metadata[file_permissions]}"
+# sudo find "${dir_metadata[directory]}" -type d -print0 | xargs -0 sudo chmod "${dir_metadata[directory_permissions]}"
+
+# --------------------------
+# Restart Apache service
+# --------------------------
 sudo service apache2 restart
