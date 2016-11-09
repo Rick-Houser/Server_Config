dab6e1a097e3a5b473a713d4fa9041b143ebb323
FrackenToaster <frakentoaster@gmail.com>
2016-11-09 12:41:43 -0800

    minor changes to array names and verifying removal code

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Package checks for services that need restarting
+# Works on Debian Jessie and newer
 sudo apt-get install needrestart
--- ricks_config_tool.sh
+++ ricks_config_tool.sh
@@ -5,58 +5,60 @@
 # See included README for instructions on usage and configuration.
 
 
-# Populate our package_install array
-declare -a package_install
+# Populate our install_list array
+declare -a install_list
 while IFS='\n' read -r value; do
-  $value >> package_install
-done < "files/packages/install.txt"
+  $value >> install_list
+done < "packages/install.txt"
 
-# Populate our package_removal array
-declare -a package_removal
+# Populate our removal_list array
+declare -a removal_list
 while IFS='\n' read -r value; do
-  $value >> package_removal
-done < "files/packages/remove.txt"
+  $value >> removal_list
+done < "packages/uninstall.txt"
 
 # Build our associative array from key val pairs in metadata.txt
 # Note: Syntax and order are important in this text file
 declare -A metadata
 while IFS== read -r key value; do
   metadata[$key]=$value
-done < "files/metadata/metadata.txt"
+done < "metadata.txt"
 
-# Check for package manager updates
-sudo apt-get update -q
+# Check for package manager updates and upgrade to latest
+sudo apt-get update && sudo apt-get upgrade
 
 # --------------------------
 # Remove Debian Packages
 # --------------------------
-# declare -a remove_packages=(
+# declare -a removal_list=(
 #   "php5"
 #   "libapache2-mod-php5"
 #   "php5-mcrypt"
 # )
-# for rm_pkg in "${remove_packages[@]}"
-# do
-#   if dpkg -l | grep "${rm_pkg}"; then
-#     sudo apt-get purge -y "${rm_pkg}"
-#     sudo rm -rf /etc/"${rm_pkg}" /var/lib/"${rm_pkg}"
-#     sudo apt-get autoremove -y
-#     sudo apt-get autoclean -y
-#   fi
-# done
+# Iterate removal_list. Remove if not already removed.
+for rm_pkg in "${removal_list[@]}"
+do
+  if dpkg -l | grep -i "${rm_pkg}"; then
+    sudo apt-get purge -y "${rm_pkg}" # Remove package and associated config files
+    sudo apt-get autoremove -y # Remove packages installed by other packages that aren't needed
+    sudo apt-get autoclean -y # Removes .deb files no longer installed.
+  fi
+done
 
 # --------------------------
 # Install Debian Packages
 # --------------------------
-declare -a install_packages=(
-  "apache2"
-  "mysql-server"
-  "php5-mysql"
-  "php5"
-  "libapache2-mod-php5"
-  "php5-mcrypt"
-)
-for in_pkg in "${install_packages[@]}"
+# declare -a install_list=(
+#   "apache2"
+#   "mysql-server"
+#   "php5-mysql"
+#   "php5"
+#   "libapache2-mod-php5"
+#   "php5-mcrypt"
+# )
+
+# Iterate install_list array. Install if not already installed.
+for in_pkg in "${install_list[@]}"
 do
   if ! dpkg -l | grep "${in_pkg}"; then
     sudo apt-get install -y "${in_pkg}"
@@ -70,7 +72,7 @@ do
   fi
 done
 
-# Replace default index.html with index.php
+# Replace default index.html with index.php if the file exists
 if [ -f "/var/www/html/index.html" ]; then
   sudo rm /var/www/html/index.html
   touch /var/www/html/index.php
@@ -82,14 +84,14 @@ fi
 
 # To change metadata for files or folders, use the following example as a guide.
 
-# Set metadata for a given file or directory
-declare -A metadata=(
-  ["permissions"]="644"
-  ["owner"]="root"
-  ["group"]="staff"
-  ["file"]="/var/www/html/index.php"
-  ["content"]='<?php header("Content-Type: text/plain"); echo "Hello, world!\n";'
-)
+# Set metadata and content for a given file or directory
+# declare -A metadata=(
+#   ["permissions"]="644"
+#   ["owner"]="root"
+#   ["group"]="staff"
+#   ["file"]="/var/www/html/index.php"
+#   ["content"]='<?php header("Content-Type: text/plain"); echo "Hello, world!\n";'
+# )
 sudo chmod "${metadata[permissions]}" "${metadata[file]}"
 sudo chown "${metadata[owner]}" "${metadata[file]}"
 sudo chgrp "${metadata[group]}" "${metadata[file]}"
@@ -99,11 +101,11 @@ sudo echo "${metadata[content]}" > "${metadata[file]}"
 # declare -A dir_metadata=(
 #   ["directory_permissions"]="755"
 #   ["file_permissions"]="644"
-#   ["directory"]="path/to/directory/" )
+#   ["directory"]="path/to/directory" )
 # sudo find "${dir_metadata[directory]}" -type f -print0 | xargs -0 sudo chmod "${dir_metadata[file_permissions]}"
 # sudo find "${dir_metadata[directory]}" -type d -print0 | xargs -0 sudo chmod "${dir_metadata[directory_permissions]}"
 
 # --------------------------
 # Restart a Given Service
 # --------------------------
-sudo service apache2 restart
+needrestart
