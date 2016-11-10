38b692164adfa6ee6fcf69a5d18a74cab270fe74
FrackenToaster <frakentoaster@gmail.com>
2016-11-09 23:19:28 -0800

    cleaned up comments in config tool

--- ricks_config_tool.sh
+++ ricks_config_tool.sh
@@ -10,11 +10,6 @@ sudo apt-get update && sudo apt-get upgrade
 # --------------------------
 # Remove Debian Packages
 # --------------------------
-# declare -a removal_list=(
-#   "php5"
-#   "libapache2-mod-php5"
-#   "php5-mcrypt"
-# )
 
 # Runs only if uninstall list is not empty
 if [ -s debian_packages/uninstall.txt ]; then
@@ -39,14 +34,6 @@ fi
 # --------------------------
 # Install Debian Packages
 # --------------------------
-# declare -a install_list=(
-#   "apache2"
-#   "mysql-server"
-#   "php5-mysql"
-#   "php5"
-#   "libapache2-mod-php5"
-#   "php5-mcrypt"
-# )
 
 # Runs only if install list is not empty
 if [ -s debian_packages/install.txt ]; then
@@ -83,7 +70,7 @@ if [ -f "/var/www/html/index.html" ]; then
 fi
 
 # Build our associative array from key val pairs in metadata.txt
-# Note: Syntax and order are important in this text file
+# Note: Separator and order are important in this text file
 
 # Runs only if uninstall list is not empty
 if [ -s metadata.txt ]; then
@@ -96,29 +83,6 @@ if [ -s metadata.txt ]; then
   sudo chgrp "${metadata[group]}" "${metadata[file]}"
   sudo echo "${metadata[content]}" > "${metadata[file]}"
 fi
-
-# To change metadata for files or folders, use the following example as a guide.
-
-# Set metadata and content for a given file or directory
-# declare -A metadata=(
-#   ["permissions"]="644"
-#   ["owner"]="root"
-#   ["group"]="staff"
-#   ["file"]="/var/www/html/index.php"
-#   ["content"]='<?php header("Content-Type: text/plain"); echo "Hello, world!\n";'
-# )
-# sudo chmod "${metadata[permissions]}" "${metadata[file]}"
-# sudo chown "${metadata[owner]}" "${metadata[file]}"
-# sudo chgrp "${metadata[group]}" "${metadata[file]}"
-# sudo echo "${metadata[content]}" > "${metadata[file]}"
-
-# Changes permissions for all files and folders within a given directory
-# declare -A dir_metadata=(
-#   ["directory_permissions"]="755"
-#   ["file_permissions"]="644"
-#   ["directory"]="path/to/directory" )
-# sudo find "${dir_metadata[directory]}" -type f -print0 | xargs -0 sudo chmod "${dir_metadata[file_permissions]}"
-# sudo find "${dir_metadata[directory]}" -type d -print0 | xargs -0 sudo chmod "${dir_metadata[directory_permissions]}"
 
 # --------------------------
 # Check for Required Restart
